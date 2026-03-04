6c8460c31cd63c5480f386fc1cfa9cdbd068252c
Ibrahim <111103567+iibarhimoo@users.noreply.github.com>
2026-03-04 08:59:40 +0300

    fix: correct import path for AuthenticationFailed

--- utils/custom_auth.py
+++ utils/custom_auth.py
@@ -1,6 +1,6 @@
 import os
 from rest_framework.authentication import BaseAuthentication
-from rest_framework import AuthenticationFailed
+from rest_framework.exceptions import AuthenticationFailed
 from django.contrib.auth.models import User
 
 class StaticAPIKeyAuthentication(BaseAuthentication):
